378dd9cbe89455a02aa7dc16061dde882db29657
xidizxc <66018531+xidizxc@users.noreply.github.com>
2022-04-07 21:34:22 +0800

    Update herWeater.py

--- herWeater.py
+++ herWeater.py
@@ -3,35 +3,83 @@
 
 import requests, json
 import os
-
+import requests
+from bs4 import BeautifulSoup
+import random
+import os
+import time
+from lxml import etree
 SCKEY=os.environ.get('SCKEY') ##Server酱推送KEY
 #SKey=os.environ.get('SKEY') #CoolPush酷推KEY
+ips = []  # 装载有效 IP
+def getIP():
+    for i in range(1, 5):
+        headers = {
+            "User-Agent": UserAgent().chrome  # chrome浏览器随机代理
+        }
+        ip_url = 'http://www.xiladaili.com/gaoni/{}/'.format(i)
+        html = requests.get(url=ip_url, headers=headers).text
+        seletor = etree.HTML(html)
+        ip_list = seletor.xpath('/html/body/div/div[3]/div[2]/table/tbody/tr/td[1]/text()')
+        for i in range(len(ip_list)):
+            ip = "http://" + ip_list[i]
+            # 设置为字典格式
+            proxies = {"http": ip}
+            try:
+                # 使用上面的IP代理请求百度，成功后状态码200
+                baidu = requests.get("http://myip.ipip.net/", proxies=proxies,timeout=3)
+                if baidu.status_code == 200:
+                    print(proxies,baidu.text)
+                    ips.append(proxies)
+            except:
+                print('错误')
+
+        print("正在准备IP代理，请稍后。。。")
+def getlovewords():
+        # getIP()
+    headers={
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Mobile Safari/537.36'
+    }
+    # 获取情话
+    texts=[]
+    for page in range(1,2):
+        time.sleep(3)
+        # proxy = ips[random.randint(0, len(ips) - 1)]
+        # print(proxy)
+        url = 'https://www.duanwenxue.com/huayu/tianyanmiyu/list_{}.html'.format(page)
+        try:
+            response = requests.get(url,headers=headers)
+            soup=BeautifulSoup(response.text,'lxml')
+            lovewordslist=soup.find('div',class_='list-short-article').find_all('a',target='_blank')
+            # print(lovewordslist)
+            texts.extend([lovewordslist[i].text for i in range(len(lovewordslist))])
+        except:
+            print("连接失败")
+    if(len(texts)==0):
+        print("情话集合为空")
+        return
+    else:
+        todaywords = texts[random.randint(0, len(texts) - 1)]  # 随机选取其中一条情话
+        return todaywords
 def get_iciba_everyday():
     icbapi = 'http://open.iciba.com/dsapi/'
     eed = requests.get(icbapi)
     bee = eed.json()  #返回的数据
     english = bee['content']
     zh_CN = bee['note']
-    str = '【奇怪的知识】\n' + english + '\n' + zh_CN
+    str = '宝贝早安~\n'
     return str
 
 def ServerPush(info): #Server酱推送
     api = "https://sc.ftqq.com/{}.send".format(SCKEY)
     title = u"天气推送"
-    content = info.replace('\n','\n\n')
+    content = info.replace('\n', '\n\n')
     data = {
         "text": title,
         "desp": content
     }
     print(content)
     requests.post(api, data=data)
-def CoolPush(info): #CoolPush酷推
-    # cpurl = 'https://push.xuthus.cc/group/'+spkey   #推送到QQ群
-    # cpurl = 'https://push.xuthus.cc/send/' + SKey  # 推送到个人QQ
-    api='https://push.xuthus.cc/send/{}'.format(SKey)
-    print(api)
-    print(info)
-    requests.post(api, info.encode('utf-8'))
 def main():
     try:
         api = 'http://t.weather.itboy.net/api/weather/city/'             #API地址，必须配合城市代码使用
@@ -57,10 +105,10 @@ def main():
             ganmao = d["data"]["ganmao"] #感冒指数
             tips = d["data"]["forecast"][0]["notice"] #温馨提示
             # 天气提示内容
-            tdwt = "【今日份天气】\n城市： " + parent + city + \
+            tdwt = "今天天气怎么样呢？\n宝贝所在城市： " + parent + city + \
                    "\n日期： " + date + "\n星期: " + week + "\n天气: " + weather_type + "\n温度: " + wendu_high + " / "+ wendu_low + "\n湿度: " + \
                     shidu + "\nPM25: " + pm25 + "\nPM10: " + pm10 + "\n空气质量: " + quality + \
-                   "\n风力风向: " + fx + fl + "\n感冒指数: "  + ganmao + "\n温馨提示： " + tips + "\n更新时间: " + update_time + "\n✁-----------------------------------------\n" + get_iciba_everyday()
+                   "\n风力风向: " + fx + fl + "\n感冒指数: "  + ganmao + "\n温馨提示： " + tips + "\n更新时间: " + update_time + "\n✁-----------------------------------------\n" + get_iciba_everyday() + getlovewords()
             # print(tdwt)
             # requests.post(cpurl,tdwt.encode('utf-8'))         #把天气数据转换成UTF-8格式，不然要报错。
             ServerPush(tdwt)
@@ -72,3 +120,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
